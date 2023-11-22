e45f2a2125afcd79833992ae4c4b66c121e80112
json-ahn <jason.ahn.js@gmail.com>
2023-11-21 18:18:35 -0800

    rps complete

--- external.js
+++ external.js
@@ -1,76 +1,143 @@
 function getComputerChoice() {
-    let hand = ["rock", "paper", "scissors"];
-    console.log(hand[(Math.floor(Math.random() * hand.length))]);
+    let random_hand = ["rock", "paper", "scissors"];
+    return random_hand[(Math.floor(Math.random() * random_hand.length))];
 }
 
-// function getPlayerChoice() {
-//     let choice = window.prompt("Select! Rock, Paper or Scissors");
-//     choice = choice.toLowerCase();
-//     return choice;
-// }
+let human = 0;
+let computer = 0;
+let total = 0;
 
-// let playerSelection = getPlayerChoice();
-// let computerSelection = getComputerChoice();
+const container = document.querySelector('.result');
+
 
 function playRound(playerSelection, computerSelection) {
+
     playerSelection = playerSelection.toLowerCase();
-    
-    if(playerSelection === "rock") {
-        if(computerSelection === "rock") {
-            return "Draw!";
-        } else if(computerSelection === "paper") {
-            return "Win!";
-        } else if(computerSelection === "scissors") {
-            return "Lose!";
+
+    // res.textContent = playerSelection;
+    // container.appendChild(res);
+
+    const res = document.createElement('p');
+    const point = document.createElement('p');
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "rock") {
+            
+            res.textContent = "It's a tie";
+            container.appendChild(res);
+        } else if (computerSelection === "paper") {
+            computer++;
+            total++;
+
+            point.textContent = computer;
+            container.appendChild(point);
+
+            
+            res.textContent = "You Lose! Paper beats Rock";
+            container.appendChild(res);
+            
+        } else if (computerSelection === "scissors") {
+            human++;
+            total++;
+
+            point.textContent = human;
+            container.appendChild(point);
+
+            res.textContent = "You Win! Rock beats Scissors";
+            container.appendChild(res);
+            
         }
-    } else if(playerSelection === "paper") {
-        if(computerSelection === "rock") {
-            return "Win!";
-        } else if(computerSelection === "paper") {
-            return "Draw!";
-        } else if(computerSelection === "scissors") {
-            return "Lose!";
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            human++;
+            total++;
+
+            point.textContent = human;
+            container.appendChild(point);
+
+            res.textContent = "You Win! Paper beats Rock";
+            container.appendChild(res);
+        } else if (computerSelection === "paper") {
+            res.textContent = "It's a tie";
+            container.appendChild(res);
+        } else if (computerSelection === "scissors") {
+            computer++;
+            total++;
+
+            point.textContent = computer;
+            container.appendChild(point);
+
+            res.textContent = "You Lose! Scissors beats Rock";
+            container.appendChild(res);
+            
         }
-    } else if(playerSelection === "scissors") {
-        if(computerSelection === "rock") {
-            return "Lose!";
-        } else if(computerSelection === "paper") {
-            return "Win!";
-        } else if(computerSelection === "scissors") {
-            return "Draw!";
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            computer++;
+            total++;
+
+            point.textContent = computer;
+            container.appendChild(point);
+
+            res.textContent = "You Lose! Rock beats Scissors";
+            container.appendChild(res);
+            
+        } else if (computerSelection === "paper") {
+            human++;
+            total++;
+
+            point.textContent = human;
+            container.appendChild(point);
+
+            res.textContent = "You Win! Scissors beats Paper";
+            container.appendChild(res);
+            
+        } else if (computerSelection === "scissors") {
+            res.textContent = "It's a tie";
+            container.appendChild(res);
+            
         }
     }
 }
+
+// const button = document.querySelector("#rock");
+// let button_specific = button.textContent;
+// button.addEventListener('click', () => {
+//     console.log(button_specific);
+// })
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(total < 5) {
+            playRound(button.id, getComputerChoice());
+        } else {
+            const final = document.createElement('p');
+            if(computer < human) {
+                final.textContent = "HUMAN WON!";
+                container.appendChild(final);
+            } else {
+                final.textContent = "COMPUTER WON!";
+                container.appendChild(final);
+            }
+        }
+    });
+});
+
+
+// button.forEach(eachButton => {
+//     eachButton.addEventListener('click', () => {
+//         console.log(eachButton.value);
+//     });
+// });
 
 // function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-
-//     for(let i = 0; i < 5; i++) {
-//         let result = playRound(playerSelection, computerSelection);
-//         console.log(result);
-//         if(result === "Win!") {
-//             playerScore++;
-//         } else if(result === "Lose!") {
-//             computerScore++;
-//         }
-//     }
-
-//     if(playerScore > computerScore) {
-//         console.log("Human won!");
-//     } else if(playerScore < computerScore) {
-//         console.log("Computer Won!");
-//     } else {
-//         console.log("It's a draw!");
-//     }
+//     const playerSelection = prompt("Rock, Paper or Scissor!");
+//     const computerSelection = getComputerChoice();
+//     console.log(playRound(playerSelection, computerSelection));
 // }
 
-function game() {
-    for(let i = 0; i < 5; i++) {
-        const playerSelection = "rock";
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-    }
-}
-
-game();
+// for(let i = 0; i < 5; i++) {
+//     game();
+// }
